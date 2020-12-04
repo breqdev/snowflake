4f4ed3338927414978d4ff363f59080071831211
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-12-03 22:28:01 -0500

    Basic SnowCloud client

---
+++ snowcloud.py
@@ -0,0 +1,71 @@
+import os
+import uuid
+import time
+import logging
+
+import requests
+
+
+class SnowCloud:
+    def __init__(self, cloud, key=None):
+        self.logger = logging.getLogger("snowcloud.SnowCloud")
+
+        self.cloud = cloud
+
+        if not key:
+            key = os.getenv("SNOWCLOUD_KEY")
+
+        self.key = key
+        self.user = str(uuid.uuid4())
+
+    def register(self):
+        self.logger.info("Registering for worker ID")
+        result = requests.post(
+            f"{self.cloud}", params={"key": self.key, "user": self.user})
+
+        result.raise_for_status()
+        result = result.json()
+
+        self.worker_id = result["worker_id"]
+        self.expires = result["expires"]
+        self.ttl = result["ttl"]
+        self.logger.info(
+            f"Registered as {self.worker_id} "
+            f"for {self.ttl} until {self.expires}")
+
+    def renew(self):
+        self.logger.info(f"Renewing registration as {self.worker_id}")
+        result = requests.post(
+            f"{self.cloud}",
+            params={
+                "key": self.key,
+                "user": self.user,
+                "renew": self.worker_id
+            }
+        )
+
+        result.raise_for_status()
+        result = result.json()
+
+        assert result["worker_id"] == self.worker_id
+        self.expires = result["expires"]
+        self.ttl = result["ttl"]
+
+        self.logger.info(
+            f"Renewed as {self.worker_id} "
+            f"for {self.ttl} until {self.expires}")
+
+    def check_renew(self):
+        if (time.time() + self.ttl/2) > self.expires:
+            self.renew()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    s = SnowCloud(os.getenv("SNOWCLOUD_URL"))
+    s.register()
+
+    while True:
+        s.check_renew()
+        time.sleep(1)
