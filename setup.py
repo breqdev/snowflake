5b47939644a14b6d3b22a61c146e3c1532288d71
Brooke Chalmers <breq@breq.dev>
2021-07-12 20:52:48 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="snowcloud",
+    version="0.1.0",
+    description="Roughly time-ordered 64-bit UUIDs",
+    author="Brooke Chalmers",
+    author_email="breq@breq.dev",
+    url="https://github.com/breq16/snowflake",
+    packages=["snowcloud"],
+    python_requires=">=3.8"
+)
