83e5972d9788740245b26b8c1e42a6858a7f1759
Brooke Chalmers <breq@breq.dev>
2022-09-03 15:53:46 -0400

    breq16 -> breqdev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     description="Roughly time-ordered 64-bit UUIDs",
     author="Brooke Chalmers",
     author_email="breq@breq.dev",
-    url="https://github.com/breq16/snowflake",
+    url="https://github.com/breqdev/snowflake",
     packages=["snowcloud"],
     python_requires=">=3.8",
     install_requires=["requests"]
